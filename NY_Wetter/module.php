7fd14c89c44ee02738f94f6614f845ad7b919921
Burki24 <burkhard@kneiseler.de>
2021-03-22 07:33:51 +0100

    Recieve Send

--- NY_Wetter/module.php
+++ NY_Wetter/module.php
@@ -26,15 +26,22 @@ declare(strict_types=1);
 		{
 			//Never delete this line!
 			parent::ApplyChanges();
-		}
+
+			$Logitude = $this->ReadPropertyString('Longitude');
+		
+					}
 
 		public function Send(string $RequestMethod, string $RequestURL, string $RequestData, int $Timeout)
 		{
+
+			$this->SendDebug('SetReceiveDataFilter - Device', $Longitude, 0);
 		//	$this->SendDataToParent(json_encode(['DataID' => '{D4C1D08F-CD3B-494B-BE18-B36EF73B8F43}', "RequestMethod" => $RequestMethod, "RequestURL" => $RequestURL, "RequestData" => $RequestData, "Timeout" => $Timeout]));
 		}
 
 		public function ReceiveData($JSONString)
 		{
+
+			$this->SetReceiveDataFilter('.*' . $Longitude . '.*');
 		//	$data = json_decode($JSONString);
 		//	IPS_LogMessage('Device RECV', utf8_decode($data->Buffer . ' - ' . $data->RequestMethod . ' - ' . $data->RequestURL . ' - ' . $data->RequestData . ' - ' . $data->Timeout));
 		}
