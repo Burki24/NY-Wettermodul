6663bad1102147817dd73e88f5f800b8cf2d98b7
Burki24 <burkhard@kneiseler.de>
2021-03-22 07:41:31 +0100

    Send/recieve update

--- NY_Wetter/module.php
+++ NY_Wetter/module.php
@@ -27,21 +27,30 @@ declare(strict_types=1);
 			//Never delete this line!
 			parent::ApplyChanges();
 
-			$Logitude = $this->ReadPropertyString('Longitude');
+			$lot = $this->ReadPropertyString('Longitude');
+			$lat = $this->ReadPropertyString('Latitude');
+			$nam = $this->ReadPropertyString('Name');
 		
 					}
 
 		public function Send(string $RequestMethod, string $RequestURL, string $RequestData, int $Timeout)
 		{
 
-			$this->SendDebug('SetReceiveDataFilter - Device', $Longitude, 0);
+			$this->SendDebug('SetReceiveDataFilter - lot', $Lot, 0);
+			$this->SendDebug('SetReceiveDataFilter - lat', $lat, 0);
+			$this->SendDebug('SetReceiveDataFilter - nam', $nam, 0);
+
+
 		//	$this->SendDataToParent(json_encode(['DataID' => '{D4C1D08F-CD3B-494B-BE18-B36EF73B8F43}', "RequestMethod" => $RequestMethod, "RequestURL" => $RequestURL, "RequestData" => $RequestData, "Timeout" => $Timeout]));
 		}
 
 		public function ReceiveData($JSONString)
 		{
 
-			$this->SetReceiveDataFilter('.*' . $Longitude . '.*');
+			$this->SetReceiveDataFilter('.*' . $lot . '.*');
+			$this->SetReceiveDataFilter('.*' . $lat . '.*');
+			$this->SetReceiveDataFilter('.*' . $nam . '.*');
+
 		//	$data = json_decode($JSONString);
 		//	IPS_LogMessage('Device RECV', utf8_decode($data->Buffer . ' - ' . $data->RequestMethod . ' - ' . $data->RequestURL . ' - ' . $data->RequestData . ' - ' . $data->Timeout));
 		}
